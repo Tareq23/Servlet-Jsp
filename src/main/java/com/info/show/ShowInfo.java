9b2f0c7a4c7810691f3a3d8553e214b1f5a7f118
Tareq23 <mdtarequlislam634@gmail.com>
2022-05-02 01:34:48 +0600

    show some info

---
+++ src/main/java/com/info/show/ShowInfo.java
@@ -0,0 +1,54 @@
+package com.info.show;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShowInfo
+ */
+@WebServlet("/helloServlet")
+public class ShowInfo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ShowInfo() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		PrintWriter pw = response.getWriter();
+		
+		String email = request.getParameter("email");
+		String name = request.getParameter("name");
+		String contact = request.getParameter("contact");
+		
+		pw.println("<h3>Hello "+name+"</h3>");
+		pw.println("<p>Your Email : "+email+"</p>");
+		pw.println("<p>TYour Contact : "+contact+"</p>");
+		
+		pw.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
