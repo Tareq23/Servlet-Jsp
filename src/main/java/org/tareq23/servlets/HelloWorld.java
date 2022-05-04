62d17eb66d93d83b49efdbe2c3a76df6dbe96cc7
Tareq23 <mdtarequlislam634@gmail.com>
2022-05-04 11:45:59 +0600

    print hello world text from servlet

---
+++ src/main/java/org/tareq23/servlets/HelloWorld.java
@@ -0,0 +1,36 @@
+package org.tareq23.servlets;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorld() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+//		System.out.println("Hello World");
+		
+		response.getWriter().println("<p>Hello World</p>");
+	}
+
+}
